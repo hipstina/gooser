e182d3b850d893dd55107186f0bcc6dce62bb72f
Christina <christinapadilla.cp@gmail.com>
2021-02-25 16:01:23 -0600

    created start up files & directories

---
+++ models/blog-post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const BlogPost = new Schema(
+  {
+    author: { type: String, required: true },
+    description: { type: String, required: true }
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('blogPosts', BlogPost)
