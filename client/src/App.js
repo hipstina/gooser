fc30b3266d03eeb8ef17c2a6112ebad72a1a2a1e
Andrew Liu <liu.andrew1988@gmail.com>
2021-02-25 22:57:06 -0500

    make axios and componentDidmount, add cors in server.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,29 @@ import Form from './screens/Form'
 import Dashboard from './screens/Dashboard'
 import { Route, Switch } from 'react-router-dom'
 
+// import axios
+import axios from 'axios'
+
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
       blogPosts: []
+    }
+  }
+
+  // add componentDidMount
+  componentDidMount() {
+    this.getAllPosts()
+  }
+
+  getAllPosts = async () => {
+    try {
+      const res = await axios.get('http://localhost:3001/api/allposts')
+      console.log(res)
+    } catch (error) {
+      throw error
     }
   }
 
