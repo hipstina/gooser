9c6e370a3e3bb70586f149199bcfd9eb0c1cb425
Christina <christinapadilla.cp@gmail.com>
2021-02-26 15:26:29 -0600

    add dot env and env vars

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const db = require('./db')
+const path = require('path')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
 const cors = require('cors')
@@ -15,5 +16,12 @@ app.use(cors())
 app.use('/api', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
 
 app.listen(PORT, () => console.log(`Get ready to go goose on: ${PORT}`))
