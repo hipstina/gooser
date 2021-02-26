bb988b72bef8f2e14d5808f641685d9605992f8a
Christina <christinapadilla.cp@gmail.com>
2021-02-26 08:33:20 -0600

    add classes for styling to dash

--- client/src/components/ClearPost.jsx
+++ client/src/components/ClearPost.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import '../screens/Dashboard.css'
 
 export default class ClearPost extends Component {
   render() {
     const { postId, onClick } = this.props
     return (
       <div>
-        <button postId={postId} onClick={onClick}>
-          Clear
+        <button postId={postId} onClick={onClick} className="delete-btn">
+          x
         </button>
       </div>
     )
