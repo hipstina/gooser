02c046c5fe2b3803bc87983b801fecf15e8096bb
Andrew Liu <liu.andrew1988@gmail.com>
2021-02-26 02:59:11 -0500

    create all posts page to show all post. add grid to css

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -1,14 +1,41 @@
 import React, { Component } from 'react'
 import './Dashboard.css'
+import axios from 'axios'
+import BlogPost from '../components/BlogPost'
 
 export default class Dashboard extends Component {
+  constructor()
+  {
+    super()
+    this.state = {
+      allPosts: []
+    }
+  }
+
+  componentDidMount () {
+  this.getAllPosts()
+  }
+
+
+  getAllPosts = async () => {
+    try {
+      const res = await axios.get('http://localhost:3001/api/allposts')
+      console.log(res)
+      this.setState({allPosts: res.data.posts})
+    } catch (error) {
+      throw error
+    }
+  }
   render() {
     return (
       <div>
         <h1 className="timeline">Dashboard</h1>
-        <div className="post">
-          <p>author</p>
-          <p>new post</p> 
+        <div className="post-body">
+          {this.state.allPosts.map((result, index) =>
+          <BlogPost
+          author={result.author} 
+          description={result.description}/>
+          )}
         </div>
 
       </div>
