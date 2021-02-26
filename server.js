4c8a746acbbdf282abf5960d3b56231acc04971f
Andrew Liu <liu.andrew1988@gmail.com>
2021-02-25 23:37:21 -0500

    server update with cors

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const routes = require('./routes')
 const db = require('./db')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
 app.use(bodyParser.json())
 app.use(logger('dev'))
+app.use(cors())
 
 app.use('/api', routes)
 
